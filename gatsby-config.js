e40eb4cfbf22def5f18f97d30567f5adc4c18684
hackon hari <hackonhari@gmail.com>
2020-08-20 16:29:22 +0530

    procution deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,42 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Pandas Eating Lots",
+    description:`This is the gatsby tutorial website builded by following each and every step in that tutorial.`,
+    author:`gatsbyjs`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
---
+++ src/components/layout.js
@@ -0,0 +1,66 @@
+import React from "react"
+import { css } from "@emotion/core"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { rhythm } from "../utils/typography"
+
+export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  return (
+    <div
+      css={css`
+        margin: 0px auto;
+        max-width: 700px;
+        padding: ${rhythm(1.6)};
+      `}
+    >
+      <Link to={`/`}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(1.3)};
+            display: inline-block;
+            font-style: normal;
+            &:hover {
+              color:darkblue;
+            }
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+      <Link
+        to={`/about`}
+        css={css`
+          float: right;
+          &:hover {
+            text-decoration: none;
+          }
+        `}
+      >
+        About
+      </Link>
+      <Link
+        to={`/my-files`}
+        css={css`
+          float: right;
+          margin-right: 10px;
+          &:hover {
+            text-decoration: none;
+          }
+        `}
+      >
+        My Files
+      </Link>
+      {children}
+    </div>
+  )
+}
